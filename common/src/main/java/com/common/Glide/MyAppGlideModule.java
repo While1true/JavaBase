22fc3e717a79b4643e4581e85ccc8c2148eb2b80
公司 <893825883@qq.com>
2018-04-09 15:32:30 +0800

    glide

--- common/src/main/java/com/common/Glide/MyAppGlideModule.java
+++ common/src/main/java/com/common/Glide/MyAppGlideModule.java
@@ -7,9 +7,11 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.load.engine.cache.DiskLruCacheFactory;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.request.RequestOptions;
 import com.common.Util.AppContext;
 
 import java.io.File;
@@ -24,7 +26,9 @@ public class MyAppGlideModule extends AppGlideModule {
     public static File CACHE_PATH = new File(AppContext.get().getCacheDir(),"glide");
     @Override
     public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
-        builder.setDiskCache(new DiskLruCacheFactory(CACHE_PATH.getAbsolutePath(),CACHE_SZIE));
+        builder.setDiskCache(new DiskLruCacheFactory(CACHE_PATH.getAbsolutePath(),CACHE_SZIE))
+        .setDefaultRequestOptions(new RequestOptions().format(DecodeFormat.PREFER_RGB_565))
+        ;
     }
 
     @Override
