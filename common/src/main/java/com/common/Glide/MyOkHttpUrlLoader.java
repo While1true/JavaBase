e3a92ecb72489a03cbb0b11004e079d842537cb5
公司 <893825883@qq.com>
2018-04-09 14:59:10 +0800

    glide

--- common/src/main/java/com/common/Glide/MyOkHttpUrlLoader.java
+++ common/src/main/java/com/common/Glide/MyOkHttpUrlLoader.java
@@ -8,8 +8,10 @@ import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.load.model.ModelLoader;
 import com.bumptech.glide.load.model.ModelLoaderFactory;
 import com.bumptech.glide.load.model.MultiModelLoaderFactory;
+import com.common.TODO.RxManager;
 import com.common.Util.LogUtil;
 import com.master.rxlib.Rx.Net.HttpsCerUtil;
+import com.master.rxlib.Rx.Net.RetrofitHttpManger;
 
 import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
@@ -23,80 +25,70 @@ import okhttp3.logging.HttpLoggingInterceptor;
  */
 public class MyOkHttpUrlLoader implements ModelLoader<GlideUrl, InputStream> {
 
-  private final Call.Factory client;
-
-  // Public API.
-  @SuppressWarnings("WeakerAccess")
-  public MyOkHttpUrlLoader(@NonNull Call.Factory client) {
-    this.client = client;
-  }
-
-  @Override
-  public boolean handles(@NonNull GlideUrl url) {
-    return true;
-  }
-
-  @Override
-  public LoadData<InputStream> buildLoadData(@NonNull GlideUrl model, int width, int height,
-                                             @NonNull Options options) {
-    return new LoadData<>(model, new OkHttpStreamFetcher(client, model));
-  }
-
-  /**
-   * The default factory for {@link MyOkHttpUrlLoader}s.
-   */
-  // Public API.
-  @SuppressWarnings("WeakerAccess")
-  public static class Factory implements ModelLoaderFactory<GlideUrl, InputStream> {
-    private static volatile Call.Factory internalClient;
     private final Call.Factory client;
 
-    private static Call.Factory getInternalClient() {
-      if (internalClient == null) {
-        synchronized (Factory.class) {
-          if (internalClient == null) {
-            HttpsCerUtil.SSLParams sslSocketFactory = HttpsCerUtil.getSslSocketFactory(null, null, null);
-            internalClient = new OkHttpClient.Builder().sslSocketFactory(sslSocketFactory.sSLSocketFactory,sslSocketFactory.trustManager)
-                    .addInterceptor(new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
-                      @Override
-                      public void log(String message) {
-                        LogUtil.Log(message);
-                      }
-                    }).setLevel(HttpLoggingInterceptor.Level.BODY))
-                    .connectTimeout(12, TimeUnit.SECONDS)
-                    .readTimeout(12, TimeUnit.SECONDS)
-                    .build();
-          }
+    // Public API.
+    @SuppressWarnings("WeakerAccess")
+    public MyOkHttpUrlLoader(@NonNull Call.Factory client) {
+        this.client = client;
+    }
+
+    @Override
+    public boolean handles(@NonNull GlideUrl url) {
+        return true;
+    }
+
+    @Override
+    public LoadData<InputStream> buildLoadData(@NonNull GlideUrl model, int width, int height,
+                                               @NonNull Options options) {
+        return new LoadData<>(model, new OkHttpStreamFetcher(client, model));
+    }
+
+    /**
+     * The default factory for {@link MyOkHttpUrlLoader}s.
+     */
+    // Public API.
+    @SuppressWarnings("WeakerAccess")
+    public static class Factory implements ModelLoaderFactory<GlideUrl, InputStream> {
+        private static volatile Call.Factory internalClient;
+        private final Call.Factory client;
+
+        private static Call.Factory getInternalClient() {
+            if (internalClient == null) {
+                synchronized (Factory.class) {
+                    if (internalClient == null) {
+                        internalClient = RxManager.getClient();
+                    }
+                }
+            }
+            return internalClient;
         }
-      }
-      return internalClient;
-    }
 
-    /**
-     * Constructor for a new Factory that runs requests using a static singleton client.
-     */
-    public Factory() {
-      this(getInternalClient());
-    }
+        /**
+         * Constructor for a new Factory that runs requests using a static singleton client.
+         */
+        public Factory() {
+            this(getInternalClient());
+        }
 
-    /**
-     * Constructor for a new Factory that runs requests using given client.
-     *
-     * @param client this is typically an instance of {@code OkHttpClient}.
-     */
-    public Factory(@NonNull Call.Factory client) {
-      this.client = client;
-    }
+        /**
+         * Constructor for a new Factory that runs requests using given client.
+         *
+         * @param client this is typically an instance of {@code OkHttpClient}.
+         */
+        public Factory(@NonNull Call.Factory client) {
+            this.client = client;
+        }
 
-    @NonNull
-    @Override
-    public ModelLoader<GlideUrl, InputStream> build(MultiModelLoaderFactory multiFactory) {
-      return new MyOkHttpUrlLoader(client);
-    }
+        @NonNull
+        @Override
+        public ModelLoader<GlideUrl, InputStream> build(MultiModelLoaderFactory multiFactory) {
+            return new MyOkHttpUrlLoader(client);
+        }
 
-    @Override
-    public void teardown() {
-      // Do nothing, this instance doesn't own the client.
+        @Override
+        public void teardown() {
+            // Do nothing, this instance doesn't own the client.
+        }
     }
-  }
 }
--- common/src/main/java/com/common/TODO/RxManager.java
+++ common/src/main/java/com/common/TODO/RxManager.java
@@ -2,22 +2,27 @@ package com.common.TODO;
 
 import com.master.rxlib.Rx.Net.RetrofitHttpManger;
 
+import okhttp3.OkHttpClient;
+
 /**
  * Created by 不听话的好孩子 on 2018/4/2.
  */
 
 public class RxManager {
-
+    private static RetrofitHttpManger manger;
     public static <T> T create(Class<T> service) {
-        return SingleHolder.manger.get().create(service);
+        return manger.get().create(service);
     }
 
-    public static RetrofitHttpManger get() {
-        return SingleHolder.manger;
-    }
-
-    private static class SingleHolder {
+    public static void init(){
         //TODO
-        private static RetrofitHttpManger manger = new RetrofitHttpManger.Builder().Builder();
+        manger=null;
+    }
+    public static RetrofitHttpManger get() {
+        return manger;
+    }
+
+    public static OkHttpClient getClient(){
+        return manger.getClient();
     }
 }
